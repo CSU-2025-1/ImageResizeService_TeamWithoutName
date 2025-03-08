bc51d944102a48450139921b8e281a67e1033f06
LeeLuiza <Laurabeck1315@gmail.com>
2025-03-08 19:54:47 +0500

    add rotation

---
+++ ImageRotationMicroservice/ImageRotationMicroservice/Controllers/RotationController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.Formats;
+
+namespace ImageRotationMicroservice.Controllers;
+
+[ApiController]
+[Route("api/rotation")]
+public class RotationController : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> RotateImage(
+        IFormFile image,
+        [FromForm] double angle)
+    {
+        try
+        {
+            using var inputStream = image.OpenReadStream();
+
+            var imageData = await Image.LoadAsync(inputStream);
+
+            var format = Image.DetectFormat(inputStream);
+
+            imageData.Mutate(x => x.Rotate((float)angle));
+
+            using var outputStream = new MemoryStream();
+            await imageData.SaveAsync(outputStream, format);
+
+            return File(outputStream.ToArray(), image.ContentType);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, $"Error processing image: {ex.Message}");
+        }
+    }
+}
