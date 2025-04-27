752c75d320a8407b7e69648e84dab6f56f66365e
Anastasiya Kuznetsova <115180166+NastyaKuznet@users.noreply.github.com>
2025-04-27 11:27:51 +0500

    Add summary in ResizeImage

---
+++ ResizeImageMicroservice/ResizeImageMicroservice/Models/ImageMessage/ImageMessage.cs
@@ -0,0 +1,58 @@
+﻿namespace ResizeImageMicroservice.Models.ImageMessage
+{
+    /// <summary>
+    /// Presents a message containing information about the image and its processing parameters.
+    /// </summary>
+    public class ImageMessage
+    {
+        /// <summary>
+        /// The unique identifier of the message.
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// The original image is in Base64 format.
+        /// </summary>
+        public string OriginalImage { get; set; }
+
+        /// <summary>
+        /// The image is to be processed (can be changed in the process). It is also available in Base64 format.
+        /// </summary>
+        public string Image { get; set; }
+
+        /// <summary>
+        /// The desired width of the resulting image.
+        /// </summary>
+        public int Width { get; set; }
+
+        /// <summary>
+        /// The desired height of the resulting image.
+        /// </summary>
+        public int Height { get; set; }
+
+        /// <summary>
+        /// Specifies whether to keep the proportions of the image when resizing.
+        /// </summary>
+        public bool PreserveAspectRatio { get; set; }
+
+        /// <summary>
+        /// The angle of rotation of the image (in degrees).
+        /// </summary>
+        public double Angle { get; set; }
+
+        /// <summary>
+        /// The desired format of the resulting image (for example, JPEG, PNG, GIF).
+        /// </summary>
+        public string Format { get; set; }
+
+        /// <summary>
+        /// Indicates whether the image size needs to be resized.
+        /// </summary>
+        public bool IsNeedResize { get; set; }
+
+        /// <summary>
+        /// Indicates whether the image needs to be rotated.
+        /// </summary>
+        public bool IsNeedRotation { get; set; }
+    }
+}
--- ResizeImageMicroservice/ResizeImageMicroservice/Services/ConsumerService.cs
+++ ResizeImageMicroservice/ResizeImageMicroservice/Services/ConsumerService.cs
@@ -1,9 +1,13 @@
 ﻿using Confluent.Kafka;
-using ResizeImageMicroservice.Kafka.Models;
-using ResizeImageMicroservice.Services;
+using ResizeImageMicroservice.Models.ImageMessage;
+using ResizeImageMicroservice.Services.Contract;
 
-namespace ResizeImageMicroservice.Kafka.Services
+namespace ResizeImageMicroservice.Services
 {
+    /// <summary>
+    /// The `ConsumerService' is a background service that consumes messages from a Kafka topic,
+    /// performs image resizing, and sends the resized image to another Kafka topic.
+    /// </summary>
     public class ConsumerService : BackgroundService
     {
         private readonly ILogger<ConsumerService> _logger;
@@ -11,9 +15,18 @@ namespace ResizeImageMicroservice.Kafka.Services
         private readonly string _topicKey = "KafkaTopics:ResizerImage";
         private readonly string _topic;
         private readonly IProducerService _producerService;
-
         private readonly IImageResizeService _imageResizeService;
 
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConsumerService"/> class.
+        /// </summary>
+        /// <param name="config">The <see cref="IConfiguration"/> interface for accessing the application configuration.</param>
+        /// <param name="logger">The interface <see cref="ILogger{ConsumerService}"/> for logging.</param>
+        /// <param name="consumerRouterImage">Interface <see cref="IConsumer{Null, ImageMessage}"/> for receiving messages from a Kafka topic.</param>
+        /// <param name="imageResizeService">The <see cref="IImageResizeService"/> interface for image resizing.</param>
+        /// <param name="producerService">The <see cref="IProducerService"/> interface for producer Kafka.</param>
+        /// <exception cref="InvalidOperationException">It is discarded if the <c>KafkaTopics key is not configured in the application configuration.:RouterImage</c>.</exception>
         public ConsumerService(
             IConfiguration config, 
             ILogger<ConsumerService> logger, 
@@ -29,6 +42,12 @@ namespace ResizeImageMicroservice.Kafka.Services
             _topic = config[_topicKey] ?? throw new InvalidOperationException($"{_topicKey} not configured in appsettings.");
         }
 
+        /// <summary>
+        /// The main method of running a background service. This method subscribes to a Kafka topic,
+        /// consumes messages, resizes the image, and sends the modified image to another topic.
+        /// </summary>
+        /// <param name="stoppingToken">The `CancellationToken' that signals the need to stop the service.</param>
+        /// <returns>A task representing an asynchronous operation.</returns>
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             _consumer.Subscribe(_topic);
@@ -87,31 +106,31 @@ namespace ResizeImageMicroservice.Kafka.Services
             }
             catch (OperationCanceledException)
             {
-                // Ensure the consumer leaves the group cleanly and final offsets are committed.
                 _consumer.Close();
             }
         }
 
+        /// <summary>
+        /// Converts a Base64 string to an 'IFormFile` object.
+        /// </summary>
+        /// <param name="base64String">The Base64 string representing the image.</param>
+        /// <returns>The <see cref="IFormFile"/> object representing the image.</returns>
         private IFormFile ConvertBase64ToIFormFile(string base64String)
         {
-            // Remove the header if it exists (e.g., "data:image/png;base64,")
             var base64Data = base64String.Substring(base64String.IndexOf(',') + 1);
-
             byte[] imageBytes = Convert.FromBase64String(base64Data);
-
-            // Create a MemoryStream from the byte array
             var memoryStream = new MemoryStream(imageBytes);
 
-            // Create a FormFile from the MemoryStream
             IFormFile formFile = new FormFile(memoryStream, 0, memoryStream.Length, "image", "image.jpg")
             {
                 Headers = new HeaderDictionary(),
-                ContentType = "image/jpeg" // Замените на фактический тип контента, если знаете его.
+                ContentType = "image/jpeg"
             };
 
             return formFile;
         }
 
+        /// <inheritdoc cref="BackgroundService.Dispose"/>
         public override void Dispose()
         {
             _consumer.Dispose();
